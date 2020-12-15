edb847ae620ab71978128311da9a4183e1ad15b5
MartinHorvath2551 <72045522+MartinHorvath2551@users.noreply.github.com>
2020-12-15 10:58:03 +0100

    Added support for hts221 sensor
    Added support for hts221 sensor
    
    Co-Authored-By: KristinaOkienkova <72023528+KristinaOkienkova@users.noreply.github.com>

---
+++ Inc/hts221.h
@@ -0,0 +1,30 @@
+/*
+ * hts221.h
+ *
+ *  Created on: Dec 15, 2020
+ *      Author: Martin
+ */
+
+#ifndef HTS221_H_
+#define HTS221_H_
+
+
+
+#endif /* HTS221_H_ */
+
+
+
+#define 	HTS221_DEVICE_ADDRESS				0x5F
+#define 	HTS221_STATUS_REG					0x27
+#define 	HTS221_WHO_AM_I						0x0F
+#define 	HTS221_WHO_AM_I_RESPONSE			0xBC
+#define 	HTS221_CTRL_REG1					0x20
+#define 	HTS221_CTRL_REG1_SETUP				0x81
+#define 	HTS221_ADRESS						0xbe
+#define 	HTS221_DEVICE_ON					1000001
+
+
+uint8_t hts221_read_byte(uint8_t reg_addr);
+void hts221_readArray(uint8_t * data, uint8_t reg, uint8_t length);
+uint8_t hts221_init(void);
+void hts221_write_byte(uint8_t reg_addr, uint8_t value);
---
+++ Src/hts221.c
@@ -0,0 +1,57 @@
+/*
+ * hts221.c
+ *
+ *  Created on: Dec 13, 2020
+ *      Author: Martin
+ */
+
+
+#include "i2c.h"
+#include <hts221.h>
+
+
+
+uint8_t hts221_read_byte(uint8_t reg_addr)
+{
+	uint8_t data = 0;
+	return *(i2c_master_read(&data, 1, reg_addr, HTS221_ADRESS, 0));
+}
+
+void hts221_readArray(uint8_t * data, uint8_t reg, uint8_t length)
+{
+	i2c_master_read(data, length, reg, HTS221_ADRESS, 1);
+}
+
+void hts221_write_byte(uint8_t reg_addr, uint8_t value)
+{
+	i2c_master_write(value, reg_addr, HTS221_ADRESS, 0);
+}
+
+uint8_t hts221_init(void)
+{
+
+	uint8_t status = 1;
+
+	//LIS3MDL_ACC_ON;
+
+	LL_mDelay(100);
+
+	uint8_t val = hts221_read_byte(HTS221_WHO_AM_I);
+
+	if(val == HTS221_WHO_AM_I_RESPONSE)
+	{
+		status = 1;
+	}
+	else			//if the device is not found on one address, try another one
+	{
+		status = 0;
+	}
+
+
+	hts221_write_byte(HTS221_CTRL_REG1, HTS221_CTRL_REG1_SETUP);
+
+	if(hts221_read_byte(HTS221_CTRL_REG1)!=HTS221_DEVICE_ON){
+		status = 0;
+		}
+	return status;
+}
